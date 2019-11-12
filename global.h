8d245e940b33299cd14a4e009365495e802cbe35
tsambou <44768351+tsambou@users.noreply.github.com>
2019-11-12 13:28:59 +0200

    Create global.h

---
+++ global.h
@@ -0,0 +1,10 @@
+// global.h
+
+extern int gCount;
+extern const int NofCh;
+extern const int NofModes;
+extern const int NofEncoders;
+
+//constexpr int NofCh = 9;
+//constexpr int NofModes = 5;
+//constexpr int NofEncoders = 3;
